4b97b884eab08296e201a7cc98d3c62045c3b11a
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2013-10-03 11:18:37 +0200

    pause stream during read, and wait for callback

--- preparedata.js
+++ preparedata.js
@@ -1,16 +1,20 @@
 // {{{1 Utility functions
 var fs;
 fs = require("fs");
+async = require("async");
 foreachLineInFile = function(filename, fn) {
   var stream;
   stream = fs.createReadStream(filename)
-  readbuf = "";
+    readbuf = "";
   stream.on("data", function(data) {
+    stream.pause()
     var lines;
-    readbuf += data;
-    lines = readbuf.split(RegExp("\\n", "g"));
-    lines.slice(0, -1).forEach(fn);
-    readbuf = lines[lines.length - 1];
+  readbuf += data;
+  lines = readbuf.split(RegExp("\\n", "g"));
+  async.each(lines.slice(0, -1), fn, function foo() { 
+    stream.resume();
+  });
+  readbuf = lines[lines.length - 1];
   });
   stream.on("end", function(){
     if(readbuf) {
@@ -29,33 +33,40 @@ lineMatch = function(line) {
   if(match) {
     return {
       patron: match[1],
-      library: match[2],
-      sex: (match[3] === "m") ? "m" : "f",
-      birthyear: match[4].slice(0,4),
-      lid: match[5],
-      date: match[6]
+        library: match[2],
+        sex: (match[3] === "m") ? "m" : "f",
+        birthyear: match[4].slice(0,4),
+        lid: match[5],
+        date: match[6]
     }
   }
 }
+lineno = 0;
 processAdhlFile = function(filename, callback, done) {
-  foreachLineInFile(filename, function(line) {
+  foreachLineInFile(filename, function(line, linedone) {
+    ++lineno;
+    if(lineno % 1000 === 0) {
+      console.log("processing line:" + lineno);
+    }
     if(line === undefined) {
       return done()
     }
     obj = lineMatch(line);
     if(obj) {
-      callback(obj);
+      callback(obj, linedone);
+    } else {
+      linedone();
     }
   });
 }
 
 // {{{2 Main dispatch
 if(process.argv[2] === "adhl" && process.argv[3]) {
-  processAdhlFile(process.argv[3], function(obj) {
-      console.log(obj);
-      }, function() {
-      console.log("done");
-      });
+  processAdhlFile(process.argv[3], function(obj, done) {
+    done();
+  }, function() {
+    console.log("done");
+  });
 } else if(process.argv[2] === "danbib") {
 } else {
   console.log("Parameter neede, - read " + __dirname + "/README.md for usage info");
